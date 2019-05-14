8b2ec69f562cd3c082edbbedc981c66c1b31d946
cecprove <47523654+cecprove@users.noreply.github.com>
2019-05-14 16:34:06 +0200

    Add files via upload

--- Dinamica e controllo/mini_main.m
+++ Dinamica e controllo/mini_main.m
@@ -8,8 +8,9 @@ clc
 % link=[0.15, 0.16, 0.15 ,0.07];
 % [JL,JM,Inerzia_link,Inerzia_motori,masse_link,masse_motori,Q,Q_dot,g0]= Inizializ_dinamica(link);
 % [B,C,G]= Calcolo_matrici_dinamica_4DoF(JL,JM,Inerzia_link,Inerzia_motori,masse_link,masse_motori,Q,Q_dot,g0);
-% load('joints.mat');
-% 
+load('joints.mat');
+
+
  prompt = 'Che simulazione vuoi vedere? [D]ynamic_model - [P]IDcontroller - [S]imulation: ';
  str = input(prompt,'s');
 
